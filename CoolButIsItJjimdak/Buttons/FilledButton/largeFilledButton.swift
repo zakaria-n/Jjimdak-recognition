843741c9fb54e8bc8ec3d271d79ec0601486c835
Zakaria <33899062+zakaria-n@users.noreply.github.com>
2019-10-13 12:35:28 +0200

    Add files via upload

---
+++ CoolButIsItJjimdak/Buttons/FilledButton/largeFilledButton.swift
@@ -0,0 +1,16 @@
+//
+//  largeFilledButton.swift
+//  Cool but is it Jjimdak?
+//
+//  Created by Zakaria on 12/10/2019.
+//  Copyright © 2019 Zakaria. All rights reserved.
+//
+
+import UIKit
+
+class largeFilledButton: filledButton {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        contentEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+    }
+}
